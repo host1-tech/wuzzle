ac2fbf03e6922f8cd7de5fdacac973e305e88bef
uno <licg9999@126.com>
2020-10-18 19:51:10 +0800

    chore: verify jest register and format

--- packages/cli/src/registers/jest__26.x/transformer.ts
+++ packages/cli/src/registers/jest__26.x/transformer.ts
@@ -1,11 +1,10 @@
-import { transform } from '../node/transform';
 import { v4 as uuidv4 } from 'uuid';
+import { transform } from '../node/transform';
 
 export function getCacheKey() {
   return uuidv4();
 }
 
 export function process(code: string, file: string): string {
-  // TODO resolve errors
   return transform(code, file);
 }
