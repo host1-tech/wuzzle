45bfb6910006d2d79f671f04c9dca78a2e40842b
uno <licg9999@126.com>
2020-10-24 17:35:38 +0800

    test: add razzle 3.x test fixture

---
+++ packages/cli/__tests__/fixtures/razzle__3.x/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hi, Razzle.</div>;
+
+export default App;
---
+++ packages/cli/__tests__/fixtures/razzle__3.x/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './App';
+
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+
+const server = express();
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', (req, res) =>
+    res.status(200).send(`
+<!doctype html>
+<html lang="">
+  <head>
+    <meta charset="utf-8" />
+    ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
+  </head>
+  <body>
+    <div id="root">${renderToString(<App />)}</div>
+    ${assets.client.js ? `<script src="${assets.client.js}"></script>` : ''}
+  </body>
+</html>`)
+  );
+
+export default server;
