c9a98cfbf398352dcbd05e865ff4e5f0254c4524
uno <licg9999@126.com>
2020-11-22 17:31:51 +0800

    fix: improve helpers diff and stringify

--- packages/helpers/src/diff.ts
+++ packages/helpers/src/diff.ts
@@ -2,13 +2,21 @@ import { diffLines } from 'diff';
 
 export default function diff(oldStr: string, newStr: string) {
   return diffLines(oldStr, newStr)
-    .map(({ value, added, removed }) =>
-      added || removed
-        ? value
-            .split('\n')
-            .map(l => `${added ? '+' : '-'}${l}`)
-            .join('\n')
-        : value
-    )
+    .map(({ value, added, removed }) => {
+      const sign = added ? '+' : removed ? '-' : '';
+
+      const isEndedWithNewline = value.endsWith('\n');
+
+      if (isEndedWithNewline) value = value.substring(0, value.length - 1);
+
+      value = value
+        .split('\n')
+        .map(l => `${sign}${l}`)
+        .join('\n');
+
+      if (isEndedWithNewline) value = value + '\n';
+
+      return value;
+    })
     .join('');
 }
--- packages/helpers/src/stringify.ts
+++ packages/helpers/src/stringify.ts
@@ -1,29 +1,24 @@
-export default function jsonStringify(o: any, space: number = 2): string {
+export default function stringify(o: any, space: number = 2): string {
   return stringFormat(JSON.stringify(o, jsonReplacer, space));
 }
 
 function jsonReplacer(k: string, v: any): any {
-  if (v instanceof RegExp) {
-    return `#RegExp ${encodeURIComponent(v.toString())}`;
-  }
-  if (v instanceof Function) {
-    return `#Function ${encodeURIComponent(v.toString())}`;
-  }
+  const targets: (keyof NodeJS.Global)[] = ['RegExp', 'Function'];
+  targets.some(t => {
+    if (v instanceof global[t]) return (v = `#${t} ${encodeURIComponent(v.toString())}`);
+  });
   return v;
 }
 
 function stringFormat(v: string): string {
+  const targets = [/"#RegExp (.+)"/, /"#Function (.+)"/];
   return v
     .split('\n')
     .map(l => {
-      const matchRegExp = l.match(/"#RegExp (.+)"/);
-      if (matchRegExp) {
-        return decodeURIComponent(matchRegExp[1]);
-      }
-      const matchFunction = l.match(/"#Function (.+)"/);
-      if (matchFunction) {
-        return decodeURIComponent(matchFunction[1]);
-      }
+      targets.some(t => {
+        const m = l.match(t);
+        if (m) return (l = l.replace(m[0], decodeURIComponent(m[1])));
+      });
       return l;
     })
     .join('\n');
