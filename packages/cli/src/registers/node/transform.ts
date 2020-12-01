5ad4de64410f3aa8a8931f0868eb98c11fa507d4
uno <licg9999@126.com>
2020-12-01 21:54:10 +0800

    refactor: rename transform-cli.ts -> convert.ts

--- packages/cli/src/registers/node/transform.ts
+++ packages/cli/src/registers/node/transform.ts
@@ -1,9 +1,9 @@
 import execa from 'execa';
 
 export function transform(code: string, file: string): string {
-  const transformCliPath = require.resolve('./transform-cli');
+  const convertPath = require.resolve('./convert');
   const nodePath = process.argv[0];
-  const { stdout, stderr } = execa.sync(nodePath, [transformCliPath, file], { input: code });
+  const { stdout, stderr } = execa.sync(nodePath, [convertPath, file], { input: code });
   process.stderr.write(stderr);
   return stdout;
 }
