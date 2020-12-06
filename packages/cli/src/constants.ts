f6eef634056c6889521e28f94dc307a1cb3690c4
uno <licg9999@126.com>
2020-12-06 14:56:41 +0800

    chore: add more env keys

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,5 +1,7 @@
 // EK is short for env key,
 // EK_ prefixed constants is used for indexing values from `process.env`
+export const EK_RPOJECT_ANCHOR = 'WUZZLE_PROJECT_ANCHOR';
 export const EK_NODE_LIKE_EXTRA_OPTIONS = 'WUZZLE_NODE_LIKE_EXTRA_OPTIONS';
-export const EK_ANCHOR_NAME = 'WUZZLE_ANCHOR_NAME';
 export const EK_COMMAND_NAME = 'WUZZLE_COMMAND_NAME';
+export const EK_COMMAND_ARGS = 'WUZZLE_COMMAND_ARGS';
+export const EK_INTERNAL_PRE_CONFIG = 'WUZZLE_INTERNAL_PRE_CONFIG';
