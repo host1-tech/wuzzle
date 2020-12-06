0d6221d8756dead6a8265ae376209bcc5d935e36
uno <licg9999@126.com>
2020-12-06 15:13:07 +0800

    chore: improve scripts/fixtures texts

--- packages/cli/scripts/fixtures.ts
+++ packages/cli/scripts/fixtures.ts
@@ -8,12 +8,12 @@ program.command('install').action(() => {
   const fixturesPath = '__tests__/fixtures';
   shelljs.cd(fixturesPath);
   shelljs.ls().forEach(dirname => {
-    console.log(blue(`Install deps in \`${dirname}\``));
+    console.log(blue(`Install deps in '${dirname}'`));
     shelljs.cd(dirname);
     if (shelljs.test('-f', 'package.json')) {
       shelljs.exec('yarn');
     } else {
-      console.log(grey(`No \`package.json\` found, skip \`${dirname}\``));
+      console.log(grey(`No 'package.json' found, skip '${dirname}'`));
     }
     shelljs.cd('..');
   });
