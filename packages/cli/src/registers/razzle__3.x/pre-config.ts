aafe7341aeee862fe16eb2a9f786c7e5cc4d1488
uno <licg9999@126.com>
2020-12-06 15:38:39 +0800

    chore: normalize webpack import

--- packages/cli/src/registers/razzle__3.x/pre-config.ts
+++ packages/cli/src/registers/razzle__3.x/pre-config.ts
@@ -1,6 +1,6 @@
 import { cosmiconfigSync } from 'cosmiconfig';
 import path from 'path';
-import type { Configuration } from 'webpack';
+import type webpack from 'webpack';
 import { merge } from 'webpack-merge';
 import { EK_COMMAND_ARGS, EK_COMMAND_NAME } from '../../constants';
 
@@ -8,7 +8,7 @@ const babelConfigExplorer = cosmiconfigSync('babel');
 const commandName = process.env[EK_COMMAND_NAME]!;
 const razzleCommand = JSON.parse(process.env[EK_COMMAND_ARGS]!)[0];
 
-export default (webpackConfig: Configuration) => {
+export default (webpackConfig: webpack.Configuration) => {
   if (commandName != 'razzle') return;
   if (razzleCommand == 'test') {
     return merge(webpackConfig, {
