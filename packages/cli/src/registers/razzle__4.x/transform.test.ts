1104dcd0d0800f799fb3e65212f103493f33ba0a
uno <licg9999@126.com>
2021-11-26 22:28:53 +0800

    fix: detect webpack version in razzle 4 handling

--- packages/cli/src/registers/razzle__4.x/transform.test.ts
+++ packages/cli/src/registers/razzle__4.x/transform.test.ts
@@ -1,6 +1,8 @@
+import { mocked } from 'ts-jest/utils';
 import { EK_COMMAND_ARGS } from '../../constants';
 import { register as registerJest26, unregister as unregisterJest26 } from '../jest__26.x';
 import { register as registerWebpack4, unregister as unregisterWebpack4 } from '../webpack__4.x';
+import { register as registerWebpack5, unregister as unregisterWebpack5 } from '../webpack__5.x';
 import { register, unregister } from './transform';
 
 const commandPath = '/path/to/command';
@@ -8,6 +10,7 @@ const commandPath = '/path/to/command';
 jest.mock('@wuzzle/helpers');
 jest.mock('../jest__26.x');
 jest.mock('../webpack__4.x');
+jest.mock('../webpack__5.x');
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -15,10 +18,19 @@ beforeEach(() => {
 });
 
 describe('register/unregister', () => {
-  it('uses webpack register on default registering', () => {
+  it('uses webpack 4 register on default registering', () => {
     process.env[EK_COMMAND_ARGS] = JSON.stringify(['build']);
     register({ commandPath });
     expect(registerWebpack4).toBeCalledWith({ commandPath });
+  });
+
+  it('uses webpack 5 register on default registering if webpack 4 registering fails', () => {
+    mocked(registerWebpack4).mockImplementationOnce(() => {
+      throw 0;
+    });
+    process.env[EK_COMMAND_ARGS] = JSON.stringify(['build']);
+    register({ commandPath });
+    expect(registerWebpack5).toBeCalledWith({ commandPath });
   });
 
   it('uses jest register on testing registering', () => {
@@ -27,9 +39,18 @@ describe('register/unregister', () => {
     expect(registerJest26).toBeCalledWith({ commandPath });
   });
 
-  it('uses webpack and jest unregister on unregistering', () => {
+  it('uses webpack 4 and jest unregister on unregistering', () => {
     unregister({ commandPath });
     expect(unregisterWebpack4).toBeCalledWith({ commandPath });
+    expect(unregisterJest26).toBeCalledWith({ commandPath });
+  });
+
+  it('uses webpack 5 and jest unregister on unregistering if webpack 4 unregistering fails', () => {
+    mocked(unregisterWebpack4).mockImplementationOnce(() => {
+      throw 0;
+    });
+    unregister({ commandPath });
+    expect(unregisterWebpack5).toBeCalledWith({ commandPath });
     expect(unregisterJest26).toBeCalledWith({ commandPath });
   });
 });
