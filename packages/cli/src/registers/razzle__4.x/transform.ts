34602ca0cf1657beacbf308e0f690f66b6509e3c
uno <licg9999@126.com>
2021-11-26 22:28:53 +0800

    fix: detect webpack version in razzle 4 handling

--- packages/cli/src/registers/razzle__4.x/transform.ts
+++ packages/cli/src/registers/razzle__4.x/transform.ts
@@ -2,17 +2,26 @@ import { EK_COMMAND_ARGS } from '../../constants';
 import { RegisterFunction } from '../../utils';
 import { register as registerJest26, unregister as unregisterJest26 } from '../jest__26.x';
 import { register as registerWebpack4, unregister as unregisterWebpack4 } from '../webpack__4.x';
+import { register as registerWebpack5, unregister as unregisterWebpack5 } from '../webpack__5.x';
 
 export const register: RegisterFunction = options => {
   const razzleCommand = JSON.parse(process.env[EK_COMMAND_ARGS]!)[0];
   if (razzleCommand === 'test') {
     registerJest26(options);
   } else {
-    registerWebpack4(options);
+    try {
+      registerWebpack4(options);
+    } catch {
+      registerWebpack5(options);
+    }
   }
 };
 
 export const unregister: RegisterFunction = options => {
-  unregisterWebpack4(options);
+  try {
+    unregisterWebpack4(options);
+  } catch {
+    unregisterWebpack5(options);
+  }
   unregisterJest26(options);
 };
