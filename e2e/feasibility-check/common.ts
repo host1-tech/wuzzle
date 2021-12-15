7977603a3dec54776ab14187e4effe16bae2446f
uno <licg9999@126.com>
2021-12-15 23:51:54 +0800

    test: add perf measure on wuzzle jest & mocha

--- e2e/feasibility-check/common.ts
+++ e2e/feasibility-check/common.ts
@@ -20,18 +20,18 @@ export interface TestCase {
 
 export type TestCasesInGroups = Record<string, Record<string, TestCase>>;
 
+export const testTimeout = 60000;
 export const globalDirName = 'test-as-global';
 export const wuzzleMountingText = 'Wuzzle process mounted in CWD:';
+export const silent = true;
 
 export function executeTests(testCasesInGroups: TestCasesInGroups): void {
-  describe.each(Object.keys(testCasesInGroups))('wuzzle %s', packageDesc => {
+  describe.each(Object.keys(testCasesInGroups))(`feasibility check on 'wuzzle %s'`, packageDesc => {
     const fixtureInfo = testCasesInGroups[packageDesc];
     const moduleName = packageDesc.split(' ')[0];
 
     Object.keys(fixtureInfo).forEach(versionFlag => {
       const {
-        only,
-        skip,
         fixtureDir,
         command,
         envOverrides,
@@ -42,6 +42,8 @@ export function executeTests(testCasesInGroups: TestCasesInGroups): void {
         testGlobal,
         testDryRun,
         testUnregister,
+        only,
+        skip,
       } = fixtureInfo[versionFlag];
       const commandName = command.split(' ')[0];
       const globalDir = path.join(fixtureDir, globalDirName);
@@ -81,76 +83,95 @@ export function executeTests(testCasesInGroups: TestCasesInGroups): void {
         });
 
         (skipNormal ? it.skip : it)(`runs ${command}`, () => {
-          const result = shelljs.exec(genEndToEndExec({ command, envOverrides }));
+          const result = shelljs.exec(genEndToEndExec({ command, envOverrides }), { silent });
           verifyCompilationResult(result);
         });
 
         if (testGlobal) {
-          it(`runs ${command} from globals`, () => {
-            shelljs.mkdir(globalDir);
-            const globalContents = glob.sync(path.join(fixtureDir, '*'), {
-              absolute: true,
-              dot: true,
-              ignore: ['**/node_modules', `**/${globalDirName}`],
-            });
-            shelljs.cp('-fr', globalContents, globalDir);
-            shelljs.cd(globalDir);
-            if (testGlobal === 'with-install') {
-              shelljs.exec('yarn --prod');
-            }
-            process.env.PATH = concatEnvPath(
-              path.join(fixtureDir, 'node_modules/.bin'),
-              process.env.PATH
-            );
-            const result = shelljs.exec(genEndToEndExec({ command, envOverrides }));
-            expect(result.stdout).toEqual(
-              expect.stringMatching(`Command '${commandName}' is resolved from globals:`)
-            );
-            verifyCompilationResult(result);
-          });
+          it(
+            `runs ${command} from globals`,
+            () => {
+              shelljs.mkdir(globalDir);
+              const globalContents = glob.sync(path.join(fixtureDir, '*'), {
+                absolute: true,
+                dot: true,
+                ignore: ['**/node_modules', `**/${globalDirName}`],
+              });
+              shelljs.cp('-fr', globalContents, globalDir);
+              shelljs.cd(globalDir);
+              if (testGlobal === 'with-install') {
+                shelljs.exec('yarn --prod', { silent });
+              }
+              process.env.PATH = concatEnvPath(
+                path.join(fixtureDir, 'node_modules/.bin'),
+                process.env.PATH
+              );
+              const result = shelljs.exec(genEndToEndExec({ command, envOverrides }), {
+                silent,
+              });
+              expect(result.stdout).toEqual(
+                expect.stringMatching(`Command '${commandName}' is resolved from globals:`)
+              );
+              verifyCompilationResult(result);
+            },
+            testTimeout
+          );
         }
 
         if (testDryRun) {
-          it(`runs ${command} in dry-run mode`, () => {
-            const {
-              stdout,
-              stderr,
-              code: exitCode,
-            } = shelljs.exec(genEndToEndExec({ command: `${command} --dry-run`, envOverrides }));
-
-            expect(exitCode).toBe(0);
-            expect(stderr).toEqual(expect.stringContaining(wuzzleMountingText));
-
-            if (outputContents) {
-              Object.keys(outputContents).forEach(outputPath => {
-                expect(glob.sync(outputPath)).toHaveLength(0);
+          it(
+            `runs ${command} in dry-run mode`,
+            () => {
+              const {
+                stdout,
+                stderr,
+                code: exitCode,
+              } = shelljs.exec(genEndToEndExec({ command: `${command} --dry-run`, envOverrides }), {
+                silent,
               });
-            }
-            if (outputMessages) {
-              outputMessages.forEach(outputMessage => {
-                expect(stdout + stderr).toEqual(expect.not.stringContaining(outputMessage));
-              });
-            }
-          });
+
+              expect(exitCode).toBe(0);
+              expect(stderr).toEqual(expect.stringContaining(wuzzleMountingText));
+
+              if (outputContents) {
+                Object.keys(outputContents).forEach(outputPath => {
+                  expect(glob.sync(outputPath)).toHaveLength(0);
+                });
+              }
+              if (outputMessages) {
+                outputMessages.forEach(outputMessage => {
+                  expect(stdout + stderr).toEqual(expect.not.stringContaining(outputMessage));
+                });
+              }
+            },
+            testTimeout
+          );
         }
 
         if (testUnregister) {
-          it(`unregisters ${commandName}`, () => {
-            expect(
-              shelljs.exec(genEndToEndExec({ command: `unregister ${commandName}`, envOverrides }))
-                .code
-            ).toBe(0);
+          it(
+            `unregisters ${commandName}`,
+            () => {
+              expect(
+                shelljs.exec(
+                  genEndToEndExec({ command: `unregister ${commandName}`, envOverrides }),
+                  { silent }
+                ).code
+              ).toBe(0);
 
-            const { stderr, code: exitCode } = shelljs.exec(
-              genEndToEndExec({
-                envOverrides: { ...envOverrides, SKIP_PREFLIGHT_CHECK: 'true' },
-                wrapper: 'yarn',
-                command,
-              })
-            );
-            expect(exitCode).toBe(0);
-            expect(stderr).not.toEqual(expect.stringContaining(wuzzleMountingText));
-          });
+              const { stderr, code: exitCode } = shelljs.exec(
+                genEndToEndExec({
+                  envOverrides: { ...envOverrides, SKIP_PREFLIGHT_CHECK: 'true' },
+                  wrapper: 'yarn',
+                  command,
+                }),
+                { silent }
+              );
+              expect(exitCode).toBe(0);
+              expect(stderr).not.toEqual(expect.stringContaining(wuzzleMountingText));
+            },
+            testTimeout
+          );
         }
       });
     });
---
+++ e2e/perf-measure/mocha.spec.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import { executeTests } from './common';
+
+executeTests({
+  ['mocha']: {
+    ['8.x']: {
+      fixtureDir: path.join(__dirname, 'fixtures/mocha__8.x'),
+      bareCommand: `mocha -r @babel/register -j 1 'src/**/*'`,
+      wuzzleCommand: `mocha -j 1 'src/**/*'`,
+      tmplContent:
+        `<% _.times(lineCount, i => { %>` +
+        `export const getGreeting<%= i %> = async () => 'Hi, <%= i %>.';
+<% }); %>`,
+      tmplTesting:
+        `import assert from 'assert';
+import * as $ from '.';
+<% _.times(lineCount, i => { %>` +
+        `it('Hi, <%= i %>.', async () => assert((await $.getGreeting<%= i %>()) === 'Hi, <%= i %>.'));
+<% }); %>`,
+      totalFileCounts: { ['only a few']: 2, ['plenty of']: 20 },
+      perSubDirFileCounts: { ['non-overlapping']: 1, ['25%-overlapping']: 2 },
+      perFileLineCounts: { ['small']: 1, ['big']: 500 },
+    },
+  },
+});
