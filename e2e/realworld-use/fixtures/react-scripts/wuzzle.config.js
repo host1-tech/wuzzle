7d76c549a9aa343db6b0eec299a2f48aad09bd21
uno <licg9999@126.com>
2022-04-02 15:59:00 +0800

    test: utilize cra config in server side

--- e2e/realworld-use/fixtures/react-scripts/wuzzle.config.js
+++ e2e/realworld-use/fixtures/react-scripts/wuzzle.config.js
@@ -2,6 +2,7 @@ const { cloneDeep, toPairs } = require('lodash');
 const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 const appPaths = require('react-scripts/config/paths');
+const webpackConfigFactory = require('react-scripts/config/webpack.config');
 const {
   deleteUseItem,
   findUseItems,
@@ -11,185 +12,99 @@ const {
   insertAfterUseItem,
   insertBeforeRule,
   replaceUseItem,
+  findRules,
 } = require('../../../../packages/wuzzle/config-tools');
 
 module.exports = {
   modify(webpackConfig, webpack, { commandName }) {
+    const reactScriptsWebpackConfig =
+      commandName === 'react-scripts' ? webpackConfig : webpackConfigFactory(process.env.NODE_ENV);
+
+    const scriptRule = firstRule(reactScriptsWebpackConfig, {
+      file: { dir: appPaths.appSrc, base: 'index.ts' },
+    });
+    // Tweak babel-loader to use external babel config
+    scriptRule.options = { ...scriptRule.options, babelrc: true, presets: [], plugins: [] };
+    // Prepend ts-loader to babel-loader to do finer control on TypeScript
+    insertAfterUseItem(
+      scriptRule,
+      { loader: 'babel-loader' },
+      {
+        loader: 'ts-loader',
+        options: { compilerOptions: { noEmit: false, transpileOnly: true } },
+      }
+    );
+
+    // Tweak postcss-loader to use external postcss config
+    findUseItems(reactScriptsWebpackConfig, { loader: 'postcss-loader' }).forEach((useItem) => {
+      delete useItem.options.plugins;
+    });
+
+    // Support less files in both global and scoped formats
     const lessOptions = { javascriptEnabled: true };
-    const staticMediaName = 'static/media/[name].[hash:8].[ext]';
 
-    if (commandName === 'react-scripts') {
-      const scriptRule = firstRule(webpackConfig, {
-        file: { dir: appPaths.appSrc, base: 'index.ts' },
-      });
-      // Tweak babel-loader to use external babel config
-      scriptRule.options = { ...scriptRule.options, babelrc: true, presets: [], plugins: [] };
-      // Prepend ts-loader to babel-loader to do finer control on TypeScript
-      insertAfterUseItem(
-        scriptRule,
-        { loader: 'babel-loader' },
-        {
-          loader: 'ts-loader',
-          options: { compilerOptions: { noEmit: false, transpileOnly: true } },
-        }
-      );
+    const scssRuleQuery = { file: { dir: appPaths.appSrc, base: 'index.scss' } };
+    const lessRule = {
+      ...cloneDeep(firstRule(reactScriptsWebpackConfig, scssRuleQuery)),
+      test: /\.(less)$/,
+      exclude: /\.module\.less$/,
+    };
+    deleteUseItem(lessRule, { loader: 'resolve-url-loader' });
+    replaceUseItem(
+      lessRule,
+      { loader: 'sass-loader' },
+      { loader: 'less-loader', options: { sourceMap: true, lessOptions } }
+    );
+    firstUseItem(lessRule, { loader: 'css-loader' }).options.importLoaders = 2;
+    insertAfterRule(reactScriptsWebpackConfig, scssRuleQuery, lessRule);
 
-      // Tweak postcss-loader to use external postcss config
-      findUseItems(webpackConfig, { loader: 'postcss-loader' }).forEach((useItem) => {
-        delete useItem.options.plugins;
-      });
+    const scssModuleRuleQuery = { file: { dir: appPaths.appSrc, base: 'index.module.scss' } };
+    const lessModuleRule = {
+      ...cloneDeep(firstRule(reactScriptsWebpackConfig, scssModuleRuleQuery)),
+      test: /\.module\.less$/,
+    };
+    deleteUseItem(lessModuleRule, { loader: 'resolve-url-loader' });
+    replaceUseItem(
+      lessModuleRule,
+      { loader: 'sass-loader' },
+      { loader: 'less-loader', options: { sourceMap: true, lessOptions } }
+    );
+    firstUseItem(lessModuleRule, { loader: 'css-loader' }).options.importLoaders = 2;
+    insertAfterRule(reactScriptsWebpackConfig, scssModuleRuleQuery, lessModuleRule);
 
-      // Support less files in both plain and module formats
-      const scssRuleQuery = { file: { dir: appPaths.appSrc, base: 'index.scss' } };
-      const lessRule = {
-        ...cloneDeep(firstRule(webpackConfig, scssRuleQuery)),
-        test: /\.(less)$/,
-        exclude: /\.module\.less$/,
-      };
-      deleteUseItem(lessRule, { loader: 'resolve-url-loader' });
-      replaceUseItem(
-        lessRule,
-        { loader: 'sass-loader' },
-        { loader: 'less-loader', options: { sourceMap: true, lessOptions } }
-      );
-      firstUseItem(lessRule, { loader: 'css-loader' }).options.importLoaders = 2;
-      insertAfterRule(webpackConfig, scssRuleQuery, lessRule);
-
-      const scssModuleRuleQuery = { file: { dir: appPaths.appSrc, base: 'index.module.scss' } };
-      const lessModuleRule = {
-        ...cloneDeep(firstRule(webpackConfig, scssModuleRuleQuery)),
-        test: /\.module\.less$/,
-      };
-      deleteUseItem(lessModuleRule, { loader: 'resolve-url-loader' });
-      replaceUseItem(
-        lessModuleRule,
-        { loader: 'sass-loader' },
-        { loader: 'less-loader', options: { sourceMap: true, lessOptions } }
-      );
-      firstUseItem(lessModuleRule, { loader: 'css-loader' }).options.importLoaders = 2;
-      insertAfterRule(webpackConfig, scssModuleRuleQuery, lessModuleRule);
-
-      // Normalize svg loading in react component and url formats
-      insertBeforeRule(
-        webpackConfig,
-        { loader: 'file-loader' },
-        {
-          test: /\.svg$/,
-          use: [
-            { loader: '@svgr/webpack' },
-            { loader: 'file-loader', options: { name: staticMediaName } },
-          ],
-        }
-      );
-    }
+    // Normalize svg loading in react component and url formats
+    const fileRule = firstRule(reactScriptsWebpackConfig, { loader: 'file-loader' });
+    insertBeforeRule(
+      reactScriptsWebpackConfig,
+      { loader: 'file-loader' },
+      {
+        test: /\.svg$/,
+        use: [{ loader: '@svgr/webpack' }, { loader: 'file-loader', options: fileRule.options }],
+      }
+    );
 
     if (commandName === 'transpile' || commandName === 'node') {
       // Setup client-compatible server-side compilation config
 
-      const fileLoadOpts = { name: staticMediaName, emitFile: false };
+      const { module, resolve, resolveLoader } = reactScriptsWebpackConfig;
 
-      const urlLoadOpts = {
-        ...fileLoadOpts,
-        limit: parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT || '10000'),
-      };
+      while (deleteUseItem(module, { loader: 'mini-css-extract-plugin' }));
+      while (deleteUseItem(module, { loader: 'style-loader' }));
 
-      const cssLoadOpts = {
-        modules: {
-          exportOnlyLocals: true,
-          getLocalIdent: getCSSModuleLocalIdent,
-        },
-      };
+      ['file-loader', 'url-loader'].forEach((loader) =>
+        [...findRules(module, { loader }), ...findUseItems(module, { loader })].forEach(
+          ({ options }) => {
+            if (options) options.emitFile = false;
+          }
+        )
+      );
 
-      const postcssLoadOpts = {
-        ident: 'postcss',
-      };
+      findUseItems(module, { loader: 'css-loader' }).forEach(({ options: { modules } }) => {
+        if (modules) modules.exportOnlyLocals = true;
+      });
 
       webpackConfig.output.publicPath = appPaths.publicUrlOrPath;
-      webpackConfig.module = {
-        rules: [
-          { parser: { requireEnsure: false } },
-          {
-            oneOf: [
-              {
-                test: /\.(js|jsx|ts|tsx)$/,
-                use: [
-                  { loader: 'babel-loader' },
-                  {
-                    loader: 'ts-loader',
-                    options: { compilerOptions: { noEmit: false } },
-                  },
-                ],
-              },
-              {
-                test: /\.css$/,
-                use: [
-                  {
-                    loader: 'css-loader',
-                    options: { importLoaders: 1, ...cssLoadOpts },
-                  },
-                  {
-                    loader: 'postcss-loader',
-                    options: postcssLoadOpts,
-                  },
-                ],
-              },
-              {
-                test: /\.(scss|sass)$/,
-                use: [
-                  {
-                    loader: 'css-loader',
-                    options: { importLoaders: 3, ...cssLoadOpts },
-                  },
-                  {
-                    loader: 'postcss-loader',
-                    options: postcssLoadOpts,
-                  },
-                  {
-                    loader: 'resolve-url-loader',
-                    options: { root: appPaths.appSrc },
-                  },
-                  { loader: 'sass-loader' },
-                ],
-              },
-              {
-                test: /\.(less)$/,
-                use: [
-                  {
-                    loader: 'css-loader',
-                    options: { importLoaders: 2, ...cssLoadOpts },
-                  },
-                  {
-                    loader: 'postcss-loader',
-                    options: postcssLoadOpts,
-                  },
-                  { loader: 'less-loader', options: { lessOptions } },
-                ],
-              },
-              {
-                test: /\.svg$/,
-                use: [
-                  { loader: '@svgr/webpack' },
-                  {
-                    loader: 'file-loader',
-                    options: fileLoadOpts,
-                  },
-                ],
-              },
-              {
-                test: /\.(bmp|gif|jpe?g|png|avif)$/,
-                loader: 'url-loader',
-                options: urlLoadOpts,
-              },
-              {
-                exclude: /\.(json)$/,
-                loader: 'file-loader',
-                options: fileLoadOpts,
-              },
-            ],
-          },
-        ],
-      };
-      webpackConfig.resolve = { extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'] };
+      Object.assign(webpackConfig, { module, resolve, resolveLoader });
     }
   },
 
